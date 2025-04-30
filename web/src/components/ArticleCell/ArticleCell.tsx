3830612923ea462d619d24489c455e517b7409b4
Andamlaka <andargachweewawey@gmail.com>
2025-05-01 02:08:50 +0300

    detail page post

---
+++ web/src/components/ArticleCell/ArticleCell.tsx
@@ -0,0 +1,41 @@
+import type { FindArticleQuery, FindArticleQueryVariables } from 'types/graphql'
+
+import type {
+  CellSuccessProps,
+  CellFailureProps,
+  TypedDocumentNode,
+} from '@redwoodjs/web'
+
+interface ArticleCellProps {
+  id: number;
+ 
+}
+
+export const QUERY: TypedDocumentNode<
+  FindArticleQuery,
+  FindArticleQueryVariables
+> = gql`
+  query FindArticleQuery($id: Int!) {
+    article: article(id: $id) {
+      id,
+      
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({
+  error,
+}: CellFailureProps<FindArticleQueryVariables>) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({
+  article,
+  
+}: CellSuccessProps<FindArticleQuery, FindArticleQueryVariables>) => {
+  return <div>{JSON.stringify(article)}</div>
+}
